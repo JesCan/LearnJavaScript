f27171e12fb5d33c86435d5d9db83608e5229407
Jesus Canales <jesuscanales@outook.com>
2018-11-14 22:14:22 -0500

    Trabajando con operadores en el laboratorio 03

---
+++ Lab03/js/operadores.js
@@ -0,0 +1,38 @@
+//Asignando valores a variables con operador de incremento
+var numUno = 10;
+++numUno; //Es lo mismo que numUno = numUno + 1
+console.log(numUno);
+
+//Asignando valores a variables con operador de decremento
+var numDos = 20;
+--numDos; //Es lo mismo que numDos = numDos - 1
+console.log(numDos);
+
+//Asignando valores booleanos o lógicos
+var estado = true;
+console.log(estado);
+
+var tipo = false;
+console.log(tipo);
+
+//Operador de negación
+console.log(!estado);
+console.log(!tipo);
+
+//Operador de residuo
+var primNum = 100;
+var segNum = 55;
+resultado =  primNum % segNum;
+console.log(resultado);
+
+//Operadores de comparación
+var numero1 = 15;
+var numero2 = 18;
+respuesta01 = numero1 < numero2;
+respuesta02 = numero1 > numero2;
+respuesta03 = numero1 == numero2;
+respuesta04 = numero1 != numero2;
+console.log(respuesta01);
+console.log(respuesta02);
+console.log(respuesta03);
+console.log(respuesta04);
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 **Laboratorios**
 - Laboratorio 01: Veremos como trabajar la impresión de datos en la consola del navegador donde se ejecutará nuestro código JavaScript.
 - Laboratorio 02: Trabajamos con variables y constante entendiendo la diferencia entre ellos así como también realizamos operaciones básicas.
+- Laboratorio 03: Trabajando con operadores y variables.
